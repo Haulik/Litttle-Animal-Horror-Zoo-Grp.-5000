d917a06e431341cc5c2589c83de287d74a5029bb
Haulik <thomasbarchager@hotmail.dk>
2018-02-28 19:53:14 +0100

    Updated

--- Game.java
+++ Game.java
@@ -3,13 +3,16 @@ import java.util.Scanner;
 public class Game{   
    public static void main(String[]args){
    
+      // der laves en ny Rabbit og en ny Snake med følgende infomationer
       Rabbit rabbit1 = new Rabbit("Jacky", "American", "Blue", "Male", 2);
       Snake snake1 = new Snake("Jonny", "Anaconda", "Black", "Male", 4);
    
+     // Der bliver oprettet en Scanner, som gør det muligt at styrre vores spil med Tal
       Scanner input = new Scanner(System.in);
       System.out.print("Welcome to Little Animal Horror Zoo Demo\nPress any number to continue\n");
       double angle = input.nextDouble();
       System.out.println("_________________________________________________");
+     // Her er vores hovedmenu, der kommer frem når vi starter spillet
       menuPrompt1();
    
    // der laves et punkt der representere kaninen
@@ -22,13 +25,15 @@ public class Game{
       snake.name = "snake";
    
       int choice;
+      // Her bliver der brugt en do/while funktion.
       do
       {
-      //menuPrompt();
+         // En Choice funktion blever taget i brug, så vi kan få systemet til at gøre noget bestem, alt efter hvilken case vi vægler at benytte.
          choice = input.nextInt();
          switch(choice)
          {
          
+            // Her står alle vores cases, som alle gør noget forskelligt
             case 1:
                System.out.println("_________________________________________________");
                System.out.println("Game Started:");
@@ -50,13 +55,6 @@ public class Game{
          
             case 4:
                System.out.println("_________________________________________________");
-            /* System.out.println(" +\"\"\"\"\"+    +\"\"\"\"\"+    +\"\"\"\"\"+    +\"\"\"\"\"+    +\"\"\"\"\"+ ");
-            System.out.println("[| o o |]  [| o o |]  [| o o |]  [| o o |]  [| o o |]");
-            System.out.println(" |  ^  |    |  ^  |    |  ^  |    |  ^  |    |  ^  | ");
-            System.out.println(" | '-' |    | '-' |    | '-' |    | '-' |    | '-' | ");
-            System.out.println(" +-----+    +-----+    +-----+    +-----+    +-----+ ");
-            System.out.println("Wait what?\n5: Go back!!!");
-            */
                System.out.println("The Rabbit starts at (0,0)\nThe Snake starts at (10,10)\nRabbits Goal: To survive for as long as you can without getting caught by the snake\nsnakes Goal: Catch the rabbit\n\n5: back");
                break;
          
@@ -108,31 +106,16 @@ public class Game{
             case 0:
                System.exit(0);
                break;
-         
-            case 12:
-            /* System.out.println(" +\"\"\"\"\"+    +\"\"\"\"\"+    +\"\"\"\"\"+    +\"\"\"\"\"+    +\"\"\"\"\"+ ");
-            System.out.println("[| o o |]  [| o o |]  [| o o |]  [| o o |]  [| o o |]");
-            System.out.println(" |  ^  |    |  ^  |    |  ^  |    |  ^  |    |  ^  | ");
-            System.out.println(" | '-' |    | '-' |    | '-' |    | '-' |    | '-' | ");
-            System.out.println(" +-----+    +-----+    +-----+    +-----+    +-----+ ");
-            System.out.println("Wait what?\n5: Go back!!!");
-            */
-               System.out.println("searching for players...");
-               System.out.println("..\n...\nNo Player online, Try again never"); 
-               System.out.println("\n5: Back");
-               break;
-          
+
+            // Her er Default mulighed som bruges, hvis man ikke trykker et tal som er oprettet som en case. 
             default:
                System.out.println("_________________________________________________");
                System.out.println("WTF ARE YOU DOING?!?!?\n5: GO BACK!");
          }
+         //Når man trykker 1 i konsolen vil Do/while funktionen stoppe og java går videre til næste punkt
       }while(choice !=1);
     
-     // metode til at udregne afstanden mellem rabbit(x,y) og snake (x,y)   
-   // http://www.mathwarehouse.com/algebra/distance_formula/index.php
-   // double distb = Math.sqrt(((snake.x - rabbit.x)*(snake.x - rabbit.x))+ ((snake.y - rabbit.y)*(snake.y-rabbit.y)));   
-   
-   
+  
       do
       {
          choice = input.nextInt();
@@ -141,15 +124,17 @@ public class Game{
          
             case 1:
                System.out.println("_________________________________________________");
+               // metode til at udregne afstanden mellem rabbit(x,y) og snake (x,y)
+               // http://www.mathwarehouse.com/algebra/distance_formula/index.php 
                double distb = Math.sqrt(((snake.x - rabbit.x)*(snake.x - rabbit.x))+ ((snake.y - rabbit.y)*(snake.y-rabbit.y)));
                rabbit.move(1,0);
                snake.move(0,-1);
                System.out.println("\n1: Move Rabbit (1,0)");
                System.out.println("2: Move Rabbit (0,1)");
                System.out.println("0: Give up");
+               
                if (distb < 2){
                   System.out.println("\nThe Snake has caught the Rabbit!\nPress 0 to continue");
-               
                
                
                }else if(distb < 3){
@@ -165,10 +150,11 @@ public class Game{
                System.out.println("\n1: Move Rabbit (1,0)");
                System.out.println("2: Move Rabbit (0,1)");
                System.out.println("0: Give up");
+               // hvis afstanden er mindre end 2, altså hvis det to punkter er ens, bliver der printet en besked ud.
                if (distb2 < 2){
                   System.out.println("\nThe Snake has caught the Rabbit!\nPress 0 to continue");
                
-               
+               // hvis afstanden mellem de to er mellem 1 og 3 er de ved siden af hinanden, og printer en ny besked ud
                }else if(distb2 < 3){
                   System.out.println("\nNOOOOO, Please don't eat me");
                }    
@@ -184,21 +170,10 @@ public class Game{
                break; 
          }
       }while(choice !=0);
-    /*
    
-   // hvis afstanden er mindre end 1, altså hvis det to punkter er ens, bliver der printet en besked ud.
-   if (distb < 1){
-   System.out.println("\nThe Snake has caught the Rabbit!\nGame over!\n\n0: Close game?\n1: Restart!"); 
-   
-    // hvis afstanden mellem de to er mellem 1 og 2 er de ved siden af hinanden, og printer en ny besked ud
-   /if (distb >=1)
-   System.out.println("NOOOOO, Please don't eat me!!!");
-   } 
-   */
    
       do
       {
-      //menuPrompt();
          choice = input.nextInt();
          switch(choice)
          {
@@ -239,17 +214,14 @@ public class Game{
     
    
       
-      
-      //Her er alle vores Menuer
+       //Her er alle vores Menuer
       
    private static void menuPrompt()
    {
       System.out.println("1: Start over");
       System.out.println("0: Close Game");
    }
-      
-      
-      
+        
    private static void menuPrompt1()
    {
       System.out.println("\n1: Start game");
